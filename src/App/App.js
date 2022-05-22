d5c04b694345addf0bd93555d28b075fb60c378f
Diegoo11 <ynoacamino@gmail.com>
2022-05-22 11:18:14 -0500

    modulo Home incorporado

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from '../Header/Header'
+import Home from '../Home/Home'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Home/>
+      <hr/>
+      <h1>Hola </h1>
     </div>
   );
 }
