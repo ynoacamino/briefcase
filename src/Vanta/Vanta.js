48ceeb9e7dab3a322578cbac5a0f9ec8ef2e9f50
Diegoo11 <ynoacamino@gmail.com>
2022-05-22 19:04:12 -0500

    Modulo de animaciones 3d funcionando (Points)

---
+++ src/Vanta/Vanta.js
@@ -0,0 +1,44 @@
+import POINTS from 'vanta/dist/vanta.net.min';
+import CLOUD1 from 'vanta/dist/vanta.clouds.min'
+import CLOUD2 from 'vanta/dist/vanta.clouds2.min'
+import RINGS from 'vanta/dist/vanta.rings.min'
+import { useEffect, useRef, useState} from 'react';
+import * as THREE from 'three';
+import './Vanta.css'
+
+const Vanta = (props) => {
+  const [vantaEffect, setVantaEffect] = useState(0)
+  const vantaRef = useRef(null)
+  useEffect(() => {
+    if(!vantaEffect) {
+      setVantaEffect(
+        POINTS({
+          el: vantaRef.current,
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.00,
+          minWidth: 200.00,
+          scale: 1.00,
+          scaleMobile: 1.00,
+          color: 0xeddbe1,
+          backgroundColor: 0x161616,
+          showDots: false,
+          THREE
+        })
+      )
+    }
+    return () => {
+      if(vantaEffect) vantaEffect.destory()
+    }
+  }, [vantaEffect])
+  return (
+    <div className='effect3d'>
+      <main className='vanta' ref={vantaRef}>
+      </main>
+      <div className='degraded'></div>
+    </div>
+  )
+}
+
+export default Vanta
