72f50e38ff6a1cd0fdd198246bbb2ed1366933ec
Diegoo11 <ynoacamino@gmail.com>
2022-05-22 19:02:13 -0500

    se uso para hacer pruebas con el modulo Vanta

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,21 @@
 import './App.css';
 import Header from '../Header/Header'
 import Home from '../Home/Home'
+import {useEffect} from 'react'
+import Vanta from '../Vanta/Vanta'
 
 function App() {
+  useEffect(() => {
+    const threeScript = document.createElement('script');
+    threeScript.setAttribute("id", "threeScript")
+    threeScript.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js")
+    document.getElementsByTagName("head")[0].appendChild(threeScript)
+    return () => {
+      if(threeScript) {
+        threeScript.remove()
+      }
+    }
+  }, [])
   return (
     <div className="App">
       <Header/>
