3b6a4851833040478fcfc8af16e0d9ed1982135e
Diegoo11 <ynoacamino@gmail.com>
2022-05-24 19:44:03 -0500

    cambio del link de la imagen 'photoPerfil'

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,7 +3,7 @@ import './Home.css';
 const Home = (props) => {
   return (
     <div className='Home'>
-      <img className= 'photoPerfil' src='https://cdn.discordapp.com/attachments/735526481618534452/977972532990849104/fotoPerfilHome_1_11zon_1.webp' alt='perfil'/>
+      <img className= 'photoPerfil' src='https://cdn.discordapp.com/attachments/772232222220615710/978805104171970621/fotoPerfilHome_1_11zon_1.webp' alt='perfil'/>
       <span className= 'nameHome name'>  Yenaro</span>
       <span className= 'lastNameHome name'>Noa Camino</span>
       <span className= 'devNameHome'>Full stack javascript developer</span>
