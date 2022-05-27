79a9f1b5bc1c67c8a7af4fa4a08b60333a9fd172
Diegoo11 <ynoacamino@gmail.com>
2022-05-26 19:34:33 -0500

    animaciones framer added

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,170 +1,87 @@
 import './Projects.css';
+import CardProject from '../CardProject/CardProject';
 
 const Projects = () => {
+  const works = [
+    {
+      name: 'Wheather App',
+      description: 'Una aplicación para conocer el clima en tiempo real en cualquier parte del mundo con una API web gratuita creada.',
+      github: 'https://github.com/Diegoo11/appClimaApi',
+      preview: 'https://diegoo11.github.io/appClimaApi/',
+      img: 'https://cdn.discordapp.com/attachments/772232222220615710/978810880458387486/apiclimaphoto_2.webp',
+      llave: '01'
+    },
+    {
+      name: 'Secret Club',
+      description: 'Casa club exclusiva donde sus miembros pueden escribir publicaciones anónimas. Y por fuera solo pueden ver la historia y preguntarse quien la escribio.',
+      github: 'https://github.com/Diegoo11/secretClub',
+      preview: 'https://enigmatic-forest-15876.herokuapp.com/',
+      img: 'https://cdn.discordapp.com/attachments/772232222220615710/979016568149270578/2022-05-25_08-10-37.gif',
+      llave: '02'
+    },
+    {
+      name: 'Rock Paper Scissors',
+      description: 'Piedra, Papel o Tijera, escoge uno y pruea tu suerte contra una computadora, intenta ganarle.',
+      github: 'https://github.com/Diegoo11/rock_paper_scissors',
+      preview: 'https://diegoo11.github.io/rock_paper_scissors/',
+      img: 'https://cdn.discordapp.com/attachments/772232222220615710/979016565683011644/2022-05-25_08-14-17.gif',
+      llave: '03'
+    },
+    {
+      name: 'Tic Tac Toe',
+      description: 'Tres es raya! Juega con un amigo e intenten hacer un una linea con su marca respectiva, si una gana reinicia y vuelve a intentarlo.',
+      github: 'https://github.com/Diegoo11/tictactoe',
+      preview: 'https://diegoo11.github.io/tictactoe/',
+      img: 'https://cdn.discordapp.com/attachments/772232222220615710/979016565964034129/2022-05-25_08-14-53.gif',
+      llave: '04'
+    },
+    {
+      name: 'Fake Facebook',
+      description: 'Aplicacion intentando recrear la red social Facebook. Se puede escribir post, añadir comentarios a esos mismos post, ver tus amigos actuales, aceptar y mandar solicitudes a otros usuarios, por ahora esta lleno de personas ficticias :c.',
+      github: 'https://github.com/Diegoo11/odin-book',
+      preview: 'https://stormy-ridge-72441.herokuapp.com/login',
+      img: 'https://cdn.discordapp.com/attachments/772232222220615710/979016566253428807/2022-05-25_08-28-52.gif',
+      llave: '05'
+    },
+    {
+      name: 'Donde esta Whali?',
+      description: 'Donde esta whali??? Insipirado en el juego ya reconocido en EEUU para mejorar la habalidad de concetracion de los niños al distindir a un personaje llamado whali de muchos eventos a su alrededor.',
+      github: 'https://github.com/Diegoo11/donde-esta-whali',
+      preview: 'https://where-is-whali.web.app/',
+      img: 'https://cdn.discordapp.com/attachments/772232222220615710/979016566941302825/2022-05-25_08-12-04.gif',
+      llave: '06'
+    },
+    {
+      name: 'Bathel Ship',
+      description: 'Seguro haz jugado este, tu y tu computadra tienen dos tableros con sus respectivos barcos ubicados en ellos, debes atacar a una casilla al azar e intentar destruir los barcos de tu enemigo.',
+      github: 'https://github.com/Diegoo11/bathelShip',
+      preview: 'https://diegoo11.github.io/bathelShip/',
+      img: 'https://cdn.discordapp.com/attachments/772232222220615710/979016567595618424/2022-05-25_08-13-30.gif',
+      llave: '07'
+    },
+    {
+      name: 'Restaurant Page',
+      description: 'Pagina de restaurante muy improvisada hecha 100% con javascript.',
+      github: 'https://github.com/Diegoo11/restaurant_page',
+      preview: 'https://diegoo11.github.io/restaurant_page/',
+      img: 'https://cdn.discordapp.com/attachments/772232222220615710/979016568426074182/2022-05-25_08-09-41_1.gif',
+      llave: '08'
+    },
+    {
+      name: 'Inventory Application',
+      description: 'Aplicacion de inventario perfecta para tiendas web, conectada a una backend para mostrarle informacion a los usuarios, y perfecta para agregar, borrar, editar categorias y articulos.',
+      github: 'https://github.com/Diegoo11/inventory',
+      preview: 'https://limitless-bayou-23084.herokuapp.com/home',
+      img: 'https://cdn.discordapp.com/attachments/772232222220615710/979016568736460890/2022-05-25_08-08-50_1.gif',
+      llave: '09'
+    }
+  ]
   return (
     <div className='Projects'>
       <span className='titleProjects'>Projects</span>
-      <div className='projectBox'>
-        <div className='leftProjectBox'>
-          <span className='nameProject'>Wheather App</span>
-          <span className='descriptionProject'>Una aplicación para conocer el clima en tiempo real en cualquier parte del mundo con una API web gratuita creada.</span>
-          <div className='btnBox'>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://github.com/Diegoo11/appClimaApi'>Revisa el codigo =&#41;</a>
-            </button>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://diegoo11.github.io/appClimaApi/'>Preview</a>
-            </button>
-          </div>
-        </div>
-        <div className='rigthProjectBox'>
-          <img className='imgProject' src='https://cdn.discordapp.com/attachments/772232222220615710/978810880458387486/apiclimaphoto_2.webp' alt='project photo preview'/>
-        </div>
-      </div>
-
-      <div className='projectBox reverse'>
-        <div className='leftProjectBox'>
-          <span className='nameProject'>Secret Club</span>
-          <span className='descriptionProject'>Casa club exclusiva donde sus miembros pueden escribir publicaciones anónimas. Y por fuera solo pueden ver la historia y preguntarse quien la escribio.</span>
-          <div className='btnBox'>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://github.com/Diegoo11/secretClub'>Revisa el codigo =&#41;</a>
-            </button>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://enigmatic-forest-15876.herokuapp.com/'>Preview</a>
-            </button>
-          </div>
-        </div>
-        <div className='rigthProjectBox'>
-          <img className='imgProject' src='https://cdn.discordapp.com/attachments/772232222220615710/979016568149270578/2022-05-25_08-10-37.gif' alt='project photo preview'/>
-        </div>
-      </div>
-
-      <div className='projectBox'>
-        <div className='leftProjectBox'>
-          <span className='nameProject'>Rock Paper Scissors</span>
-          <span className='descriptionProject'>Piedra, Papel o Tijera, escoge uno y pruea tu suerte contra una computadora, intenta ganarle.</span>
-          <div className='btnBox'>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://github.com/Diegoo11/rock_paper_scissors'>Revisa el codigo =&#41;</a>
-            </button>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://diegoo11.github.io/rock_paper_scissors/'>Preview</a>
-            </button>
-          </div>
-        </div>
-        <div className='rigthProjectBox'>
-          <img className='imgProject' src='https://cdn.discordapp.com/attachments/772232222220615710/979016565683011644/2022-05-25_08-14-17.gif' alt='project photo preview'/>
-        </div>
-      </div>
-
-      <div className='projectBox reverse'>
-        <div className='leftProjectBox'>
-          <span className='nameProject'>Tic Tac Toe</span>
-          <span className='descriptionProject'>Tres es raya! Juega con un amigo e intenten hacer un una linea con su marca respectiva, si una gana reinicia y vuelve a intentarlo.</span>
-          <div className='btnBox'>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://github.com/Diegoo11/tictactoe'>Revisa el codigo =&#41;</a>
-            </button>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://diegoo11.github.io/tictactoe/'>Preview</a>
-            </button>
-          </div>
-        </div>
-        <div className='rigthProjectBox'>
-          <img className='imgProject' src='https://cdn.discordapp.com/attachments/772232222220615710/979016565964034129/2022-05-25_08-14-53.gif' alt='project photo preview'/>
-        </div>
-      </div>
-
-      <div className='projectBox'>
-        <div className='leftProjectBox'>
-          <span className='nameProject'>Fake Facebook</span>
-          <span className='descriptionProject'>Aplicacion intentando recrear la red social Facebook. Se puede escribir post, añadir comentarios a esos mismos post, ver tus amigos actuales, aceptar y mandar solicitudes a otros usuarios, por ahora esta lleno de personas ficticias :c.</span>
-          <div className='btnBox'>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://github.com/Diegoo11/odin-book'>Revisa el codigo =&#41;</a>
-            </button>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://stormy-ridge-72441.herokuapp.com/login'>Preview</a>
-            </button>
-          </div>
-        </div>
-        <div className='rigthProjectBox'>
-          <img className='imgProject' src='https://cdn.discordapp.com/attachments/772232222220615710/979016566253428807/2022-05-25_08-28-52.gif' alt='project photo preview'/>
-        </div>
-      </div>
-
-      <div className='projectBox reverse'>
-        <div className='leftProjectBox'>
-          <span className='nameProject'>Donde esta Whali?</span>
-          <span className='descriptionProject'>Donde esta whali??? Insipirado en el juego ya reconocido en EEUU para mejorar la habalidad de concetracion de los niños al distindir a un personaje llamado whali de muchos eventos a su alrededor.</span>
-          <div className='btnBox'>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://github.com/Diegoo11/donde-esta-whali'>Revisa el codigo =&#41;</a>
-            </button>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://where-is-whali.web.app/'>Preview</a>
-            </button>
-          </div>
-        </div>
-        <div className='rigthProjectBox'>
-          <img className='imgProject' src='https://cdn.discordapp.com/attachments/772232222220615710/979016566941302825/2022-05-25_08-12-04.gif' alt='project photo preview'/>
-        </div>
-      </div>
-
-      <div className='projectBox'>
-        <div className='leftProjectBox'>
-          <span className='nameProject'>Bathel Ship</span>
-          <span className='descriptionProject'>Seguro haz jugado este, tu y tu computadra tienen dos tableros con sus respectivos barcos ubicados en ellos, debes atacar a una casilla al azar e intentar destruir los barcos de tu enemigo.</span>
-          <div className='btnBox'>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://github.com/Diegoo11/bathelShip'>Revisa el codigo =&#41;</a>
-            </button>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://diegoo11.github.io/bathelShip/'>Preview</a>
-            </button>
-          </div>
-        </div>
-        <div className='rigthProjectBox'>
-          <img className='imgProject' src='https://cdn.discordapp.com/attachments/772232222220615710/979016567595618424/2022-05-25_08-13-30.gif' alt='project photo preview'/>
-        </div>
-      </div>
-
-      <div className='projectBox reverse'>
-        <div className='leftProjectBox'>
-          <span className='nameProject'>Restaurant Page</span>
-          <span className='descriptionProject'>Pagina de restaurante muy improvisada hecha 100% con javascript.</span>
-          <div className='btnBox'>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://github.com/Diegoo11/restaurant_page'>Revisa el codigo =&#41;</a>
-            </button>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://diegoo11.github.io/restaurant_page/'>Preview</a>
-            </button>
-          </div>
-        </div>
-        <div className='rigthProjectBox'>
-          <img className='imgProject' src='https://cdn.discordapp.com/attachments/772232222220615710/979016568426074182/2022-05-25_08-09-41_1.gif' alt='project photo preview'/>
-        </div>
-      </div>
-
-      <div className='projectBox'>
-        <div className='leftProjectBox'>
-          <span className='nameProject'>Inventory Application</span>
-          <span className='descriptionProject'>Aplicacion de inventario perfecta para tiendas web, conectada a una backend para mostrarle informacion a los usuarios, y perfecta para agregar, borrar, editar categorias y articulos.</span>
-          <div className='btnBox'>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://github.com/Diegoo11/inventory'>Revisa el codigo =&#41;</a>
-            </button>
-            <button className='btn btnProjeact'>
-              <a className='link' href='https://limitless-bayou-23084.herokuapp.com/home'>Preview</a>
-            </button>
-          </div>
-        </div>
-        <div className='rigthProjectBox'>
-          <img className='imgProject' src='https://cdn.discordapp.com/attachments/772232222220615710/979016568736460890/2022-05-25_08-08-50_1.gif' alt='project photo preview'/>
-        </div>
-      </div>
+      {works.map((x) => {
+        return <CardProject key={x.llave} name={x.name} description={x.description} github={x.github} preview={x.preview} img={x.img}/>
+      })}
     </div>
   )
 }
