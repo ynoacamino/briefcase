8f9d5d31fa6e1daae6764220a77466a1a08ba7f1
Diegoo11 <ynoacamino@gmail.com>
2022-05-28 15:13:11 -0500

    code for module ViweCode

---
+++ src/ViewCode/ViewCode.js
@@ -0,0 +1,13 @@
+import './ViewCode.css';
+
+const ViewCode = () => {
+  return (
+    <div className='ViewCode'>
+      <span className='mainViewCode'>Gracias por visitar</span>
+      <span className='sideViewCode'>Puedes ver el codigo <a href='https://github.com/Diegoo11/briefcase' className='linkViewCode'>Aqui</a></span>
+      <span className='simbolViewCode'>♥️</span>
+    </div>
+  )
+}
+
+export default ViewCode
